5b461e0847885a6fadb46ec627f74c0d1a8ef9ad
Amr Moussa <amrmoussa100@gmail.com>
2021-11-11 20:43:13 -0500

    Added a clock publishing script

--- Runtime/Scripts/ROSClock.cs
+++ Runtime/Scripts/ROSClock.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.BuiltinInterfaces;
+using RosMessageTypes.Rosgraph;
 
 public class ROSClock : MonoBehaviour
 {
+    private ROSConnection ros;
+    private TimeMsg timeStamp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<ClockMsg>("clock");
+        timeStamp = new TimeMsg(0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeStamp = new TimeMsg((uint)Mathf.FloorToInt(Time.time),
+                                (uint)Mathf.FloorToInt((Time.time - Mathf.FloorToInt(Time.time)) * 1e9f));
+        ros.Publish("clock", new ClockMsg(timeStamp));
     }
+
+    public TimeMsg getTimeStamp()
+    {
+        return timeStamp;
+    }
+
+    
 }
